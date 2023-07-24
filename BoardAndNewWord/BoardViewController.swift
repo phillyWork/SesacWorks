960315dec746f7633eb12c357e246a01ba8015e5
phillyWork <hdsimonphilly@gmail.com>
2023-07-24 17:13:18 +0900

    Board Project Done (for now)

---
+++ BoardAndNewWord/BoardViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ViewController.swift
+//  BoardAndNewWord
+//
+//  Created by Heedon on 2023/07/24.
+//
+
+import UIKit
+
+class BoardViewController: UIViewController {
+
+    //MARK: - Properties
+    
+    @IBOutlet var backgroundView: UIView!
+    @IBOutlet var inputTextField: UITextField!
+    @IBOutlet var confirmButton: UIButton!
+    @IBOutlet var randomColorButton: UIButton!
+    @IBOutlet var resultLabel: UILabel!
+    
+    //MARK: - Setup UI
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configUI()
+    }
+    
+    func configUI() {
+        view.backgroundColor = .black
+        backgroundView.backgroundColor = .black
+        configTextField()
+        configButtons()
+        configLabel()
+    }
+
+    func configTextField() {
+        inputTextField.backgroundColor = .white
+        inputTextField.textColor = .black
+        inputTextField.font = .systemFont(ofSize: 16, weight: .medium)
+        inputTextField.placeholder = "텍스트를 입력하세요."
+    }
+    
+    func configButtons() {
+        configConfirmButton()
+        configRandomColorButton()
+    }
+    
+    func configConfirmButton() {
+        confirmButton.setTitleColor(.black, for: .normal)
+        confirmButton.backgroundColor = .white
+        confirmButton.clipsToBounds = true
+        confirmButton.layer.cornerRadius = 5
+        confirmButton.layer.borderColor = UIColor.black.cgColor
+        confirmButton.layer.borderWidth = 1
+    }
+    
+    func configRandomColorButton() {
+        randomColorButton.setTitleColor(.systemRed, for: .normal)
+        randomColorButton.backgroundColor = .white
+        randomColorButton.clipsToBounds = true
+        randomColorButton.layer.cornerRadius = 5
+        randomColorButton.layer.borderColor = UIColor.black.cgColor
+        randomColorButton.layer.borderWidth = 1
+    }
+    
+    func configLabel() {
+        resultLabel.font = .boldSystemFont(ofSize: 45)
+        resultLabel.textColor = .systemRed
+        resultLabel.textAlignment = .center
+        resultLabel.backgroundColor = .black
+        resultLabel.isHidden = true
+    }
+    
+    //MARK: - API
+
+    //엔터키 눌러 keyboard 내려갈 때 호출
+    @IBAction func textFieldEnterKeyTapped(_ sender: UITextField) {
+        //엔터키 누르면 확인 버튼 누른것과 동일한 효과 주기
+        confirmButtonTapped(confirmButton)
+    }
+    
+    //실질적으로 textField의 입력값 확인해서 label에 보여주기
+    @IBAction func confirmButtonTapped(_ sender: UIButton) {
+        //키보드 내리기
+        view.endEditing(true)
+        
+        //입력값 없다면 입력값 필요함 alert
+        guard inputTextField.hasText else {
+            let alert = UIAlertController(title: "입력 오류", message: "텍스트를 입력해야 입력값이 나타납니다.", preferredStyle: .alert)
+            let confirm = UIAlertAction(title: "확인", style: .cancel)
+            alert.addAction(confirm)
+            DispatchQueue.main.async {
+                self.present(alert, animated: true)
+            }
+            return
+        }
+        
+        //입력값 있으므로 결과값 보여주기
+        resultLabel.text = inputTextField.text
+        resultLabel.isHidden = false
+    }
+    
+    //randomColorButton과 resultLabel 컬러 변경하기
+    @IBAction func randomColorButtonTapped(_ sender: UIButton) {
+        let cgRed = CGFloat.random(in: 0...1)
+        let cgGreen = CGFloat.random(in: 0...1)
+        let cgBlue = CGFloat.random(in: 0...1)
+        let alpha = CGFloat.random(in: 0...1)
+        let randomColor = UIColor(cgColor: CGColor(red: cgRed, green: cgGreen, blue: cgBlue, alpha: alpha))
+        
+        randomColorButton.setTitleColor(randomColor, for: .normal)
+        resultLabel.textColor = randomColor
+    }
+    
+    @IBAction func areaTapped(_ sender: UITapGestureRecognizer) {
+        //이외 영역 누르면 키보드 내리기
+        view.endEditing(true)
+        //상위view isHidden -> 하위view도 같이 hidden
+        backgroundView.isHidden.toggle()
+        
+    }
+    
+}
+
---
+++ BoardAndNewWord/NewWordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewWordViewController.swift
+//  BoardAndNewWord
+//
+//  Created by Heedon on 2023/07/24.
+//
+
+import UIKit
+
+class NewWordViewController: UIViewController {
+
+    //MARK: - Properties
+    @IBOutlet var userTextField: UITextField!
+    @IBOutlet var searchButton: UIButton!
+    
+    
+    
+    
+    //MARK: - Setup UI
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+    
+
+    //MARK: - API
+
+}
