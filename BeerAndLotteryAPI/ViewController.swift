ddf9bb172fe0cd71234599a5d9594b86460c7a19
philly___ham (함희돈) <98685150+phillyWork@users.noreply.github.com>
2023-08-07 15:55:41 +0900

    Initial Commit

---
+++ BeerAndLotteryAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeerAndLotteryAPI
+//
+//  Created by Heedon on 2023/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
