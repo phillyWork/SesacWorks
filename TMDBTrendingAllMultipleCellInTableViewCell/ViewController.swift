e32fec69e24b4c0f50631485283be5c21cb1e72c
philly___ham (함희돈) <98685150+phillyWork@users.noreply.github.com>
2023-09-03 22:10:52 +0900

    Initial Commit

---
+++ TMDBTrendingAllMultipleCellInTableViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDBTrendingAllMultipleCellInTableViewCell
+//
+//  Created by Heedon on 2023/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
