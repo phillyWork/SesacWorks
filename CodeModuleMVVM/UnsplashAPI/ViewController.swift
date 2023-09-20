de0e4b09779d5274f5079fcf7309a69e380a67c9
philly___ham (함희돈) <98685150+phillyWork@users.noreply.github.com>
2023-09-20 12:29:07 +0900

    viewModel for non-UI functionality (VC for UI-functionality)

--- CodeModuleMVVM/UnsplashAPI/ViewController.swift
+++ CodeModuleMVVM/UnsplashAPI/ViewController.swift
@@ -16,7 +16,10 @@ class ViewController: UIViewController {
     let networkBasic = NetworkBasic.shared
     
     //generic 활용 메서드 호출하기
-    let network = Network.shared
+//    let network = Network.shared
+    
+    //viewModel에서 처리하도록 만들기
+    let viewModel = ViewModel()
     
     //imageView 확대, 축소: scrollView 내부 위치
     private lazy var scrollView: UIScrollView = {
@@ -60,7 +63,14 @@ class ViewController: UIViewController {
         //gesture 기반 action 설정
         configureGesture()
         
-        request()
+        
+        //viewModel 활용, URL 받아와서 image 할당하기
+        //VC: 어떤 이미지, 어떤 url이 왔는지 몰라도 됨
+        viewModel.request { url in
+            self.imageView.kf.setImage(with: url)
+        }
+//        request()
+        
         
 //        callRequest(query: "sky")
 //        callRandomReqeust()
@@ -134,31 +144,32 @@ class ViewController: UIViewController {
         
     }
 
-    func request() {
-        //Router 활용: 구현부 달라지는 것은 없음
-        network.requestConvertible(type: PhotoResult.self, api: .random) { response in
-            switch response {
-            case .success(let success):
-                dump(success)
-                
-                //link 활용해서 image 가져오기
-                self.imageView.kf.setImage(with: URL(string:success.urls.thumb)!)
-                
-            case .failure(let failure):
-                print(failure.errorDescription)
-            }
-        }
-    }
+    //viewModel로 이동
+//    func request() {
+//        //Router 활용: 구현부 달라지는 것은 없음
+//        network.requestConvertible(type: PhotoResult.self, api: .random) { response in
+//            switch response {
+//            case .success(let success):
+//                dump(success)
+//
+//                //link 활용해서 image 가져오기
+//                self.imageView.kf.setImage(with: URL(string:success.urls.thumb)!)
+//
+//            case .failure(let failure):
+//                print(failure.errorDescription)
+//            }
+//        }
+//    }
     
     
-    func configureHierarchy() {
+    private func configureHierarchy() {
         view.addSubview(scrollView)
         
         //imageView 확대, 축소: scrollView 내부 존재
         scrollView.addSubview(imageView)
     }
     
-    func configureLayout() {
+    private func configureLayout() {
         scrollView.snp.makeConstraints { make in
             make.center.equalTo(view.safeAreaLayoutGuide)
             make.size.equalTo(200)
@@ -171,7 +182,7 @@ class ViewController: UIViewController {
         
     }
     
-    func configureGesture() {
+    private func configureGesture() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(doubleTapGesture))
         
         //tap 횟수 요구
@@ -180,7 +191,7 @@ class ViewController: UIViewController {
         imageView.addGestureRecognizer(tap)
     }
     
-    @objc func doubleTapGesture() {
+    @objc private func doubleTapGesture() {
         //zoomScale property 기반 처리
         if scrollView.zoomScale == 1 {
             //줌 인
