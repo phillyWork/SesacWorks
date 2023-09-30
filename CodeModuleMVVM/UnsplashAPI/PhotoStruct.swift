ff4a9eca142af31fe29316420a828d3672cfff56
philly___ham (함희돈) <98685150+phillyWork@users.noreply.github.com>
2023-09-30 23:49:44 +0900

    collection view compositional dynamic height/width layout with diffable datasource

--- CodeModuleMVVM/UnsplashAPI/PhotoStruct.swift
+++ CodeModuleMVVM/UnsplashAPI/PhotoStruct.swift
@@ -10,19 +10,26 @@ import Foundation
 //Unsplash 데이터 필요한 정보만 받기
 
 //Codable: Decodable + Encodable
-struct Photo: Decodable {
+
+//Hashable: Diffable에서 Item 고유성 확보 목적
+
+struct Photo: Decodable, Hashable {
     let total: Int
     let total_pages: Int
     let results: [PhotoResult]
 }
 
-struct PhotoResult: Decodable {
+struct PhotoResult: Decodable, Hashable {
     let id: String
     let created_at: String
     let urls: PhotoURL
+    
+    //새로 추가: 각 cell마다 나타날 이미지 estimated 높이 설정 필요 ~ 비율 계산 필요
+    let width: CGFloat
+    let height: CGFloat
 }
 
-struct PhotoURL: Decodable {
+struct PhotoURL: Decodable, Hashable {
     let full: String
     let thumb: String
 }
