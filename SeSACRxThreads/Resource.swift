bc02f235e856a8134dd47f47e5c5706dce0ec18a
philly___ham (함희돈) <98685150+phillyWork@users.noreply.github.com>
2023-11-01 23:06:28 +0900

    PhoneViewController with RxSwift - PhoneNumber for emit & receive new data to update - PhoneNumber for counting string to pass Bool type and UIColor - ButtonColor to apply ButtonBackgroundColor and TextFieldTintColor - ButtonColor to apply borderColor as CGColor - IsButtonEnabled to be set as value for button

--- SeSACRxThreads/Resource.swift
+++ SeSACRxThreads/Resource.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 enum Color {
+    
+    //Xcode 15부터 활용 가능
     static let black: UIColor = .textPoint
     static let white: UIColor = .viewPoint
 }
--- SeSACRxThreads/SignUpOptional/PhoneViewController.swift
+++ SeSACRxThreads/SignUpOptional/PhoneViewController.swift
@@ -6,14 +6,26 @@
 //
  
 import UIKit
+
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class PhoneViewController: UIViewController {
    
     let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
     let nextButton = PointButton(title: "다음")
     
+    //TextField 입력값 받아 data update --> 다시 TextField에 전달
+    let phoneNumber = BehaviorSubject(value: "010")
     
+    //조건 여부 따라 달라지는 버튼 색상
+    let buttonColor = BehaviorSubject(value: UIColor.red)
+    
+    //버튼 활성화 여부 결정 data
+    let isButtonEnabled = BehaviorSubject(value: false)
+    
+    var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +34,63 @@ class PhoneViewController: UIViewController {
         
         configureLayout()
         
+        bind()
+        
         nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
     }
+    
+    func bind() {
+
+        //해당 값 textField에 나타내기
+        phoneNumber
+            .bind(to: phoneTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+        phoneNumber
+            //Rx: closure 처리 --> 메모리 누수 고려
+            //UI update: main thread 보장 필요
+            .observe(on: MainScheduler.instance)
+            //자리수 따지기: String --> Bool로 타입 변환
+            .map { $0.count > 10 }
+            .subscribe(with: self) { owner, value in
+                let color = value ? UIColor.blue : UIColor.red
+                //해당 색상 버튼 컬러에 전달
+                owner.buttonColor.onNext(color)
+                //활성화 여부 전달
+                owner.isButtonEnabled.onNext(value)
+            }
+            .disposed(by: disposeBag)
+        
+            
+        //해당 color를 button과 textField tintColor에 적용
+        buttonColor
+            .bind(to: nextButton.rx.backgroundColor, phoneTextField.rx.tintColor)
+            .disposed(by: disposeBag)
+        
+        //CGColor: 다른 타입
+        //map으로 타입 변화
+        buttonColor
+            .map { $0.cgColor }
+            .bind(to: phoneTextField.layer.rx.borderColor)
+            .disposed(by: disposeBag)
+        
+        //phoneNumber로부터 처리된 Bool data 기반으로 button 활성화 결정
+        isButtonEnabled
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        //textField 내 입력값 변화하면 다시 phone에게 전달하기
+        //orEmpty: nil값 처리 operator
+        phoneTextField.rx.text.orEmpty
+            .subscribe { value in
+                //중간 - 입력하기 (숫자만 입력, 다른 String값 입력 막기)
+                let result = value.formated(by: "###-####-####")
+                self.phoneNumber.onNext(result)
+            }
+            .disposed(by: disposeBag)
+        
+    }
+    
     
     @objc func nextButtonClicked() {
         navigationController?.pushViewController(NicknameViewController(), animated: true)
