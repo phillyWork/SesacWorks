0e349acdec9daf29542fe4462fa1d4688e334afd
philly___ham (함희돈) <98685150+phillyWork@users.noreply.github.com>
2023-11-10 10:48:12 +0900

    - analytics and crashlytics test

--- firebaseAnalyticsCrashlytics/ViewController.swift
+++ firebaseAnalyticsCrashlytics/ViewController.swift
@@ -12,8 +12,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //비정상 종료 위한 버튼
+        let button = UIButton(type: .roundedRect)
+        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+        button.setTitle("Test Crash", for: [])
+        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+        view.addSubview(button)
     }
 
+    @objc func crashButtonTapped(_ sender: AnyObject) {
+        let numbers = [0]
+        let _ = numbers[1]
+    }
 
 }
 
