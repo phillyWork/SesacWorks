8dc5f7d16cea696e8e15ebcd56b4b4e3b22534b7
philly___ham (함희돈) <98685150+phillyWork@users.noreply.github.com>
2023-11-13 09:43:55 +0900

    Initial Commit

---
+++ SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by Heedon on 2023/11/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
