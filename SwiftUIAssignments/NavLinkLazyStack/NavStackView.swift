c80702fe7bc2f2eb55ea82dcbed43b6939ce44d5
philly___ham (함희돈) <98685150+phillyWork@users.noreply.github.com>
2023-11-18 01:57:38 +0900

    - navigationlink and navigationview to push - lazyhstack with scrollview to show each section with scrollable

--- SwiftUIAssignments/NavLinkLazyStack/NavStackView.swift
+++ SwiftUIAssignments/NavLinkLazyStack/NavStackView.swift
@@ -7,11 +7,151 @@
 
 import SwiftUI
 
+struct Movie: Hashable, Identifiable {
+    let id = UUID()
+    let title: String
+    let count = Int.random(in: 1...100)
+}
+
+
 struct NavStackView: View {
+    
+    @State private var searchQuery = ""
+    
+    let movieStructArray = [Movie(title: "하나 그리고 둘"),
+                            Movie(title: "투모로우"),
+                            Movie(title: "반지의 제왕"),
+                            Movie(title: "어벤져스")
+    
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+            
+            ScrollView {
+                SectionHeaderView(text: "떠오로는 영화")
+                ScrollView(.horizontal) {
+                    LazyHStack(alignment: .center) {
+                        ForEach(movieStructArray, id: \.id) { item in
+                            NavigationLink {
+                                SampleMovieView(movie: item)
+                            } label: {
+                                VStack {
+                                    Image(systemName: "star.fill")
+                                    Text(item.title)
+                                }
+                                .frame(height: 60)
+                                .padding()
+                                .background(Color.red)
+                            }
+                        }
+                    }
+                    .background(Color.brown)
+                }
+                
+                SectionHeaderView(text: "최근 영화")
+                ScrollView(.horizontal) {
+                    LazyHStack(alignment: .center) {
+                        ForEach(movieStructArray, id: \.id) { item in
+                            NavigationLink {
+                                SampleMovieView(movie: item)
+                            } label: {
+                                VStack {
+                                    Image(systemName: "star.fill")
+                                    Text(item.title)
+                                }
+                                .frame(height: 60)
+                                .padding()
+                                .background(Color.red)
+                            }
+                        }
+                    }
+                }
+                
+                SectionHeaderView(text: "명작 추천")
+                ScrollView(.horizontal) {
+                    LazyHStack(alignment: .center) {
+                        ForEach(movieStructArray, id: \.id) { item in
+                            NavigationLink {
+                                SampleMovieView(movie: item)
+                            } label: {
+                                VStack {
+                                    Image(systemName: "star.fill")
+                                    Text(item.title)
+                                }
+                                .frame(height: 60)
+                                .padding()
+                                .background(Color.red)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("영화 목록")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Button {
+                        print("First ToolBar tapped")
+                    } label: {
+                        VStack {
+                            Image(systemName: "star.fill")
+                            Text("첫번째 툴바 탭")
+                        }
+                    }
+                }
+                ToolbarItem(placement: .bottomBar) {
+                    Button {
+                        print("Second ToolBar tapped")
+                    } label: {
+                        VStack {
+                            Image(systemName: "star.fill")
+                            Text("두번째 툴바 탭")
+                        }
+                    }
+                }
+                ToolbarItem(placement: .bottomBar) {
+                    Button {
+                        print("Third ToolBar tapped")
+                    } label: {
+                        VStack {
+                            Image(systemName: "star.fill")
+                            Text("세번째 툴바 탭")
+                        }
+                    }
+                }
+            }
+            .searchable(text: $searchQuery, placement: .navigationBarDrawer, prompt: "검색하기")
+            
+        }
+        
+        
+    }
+    
+}
+
+
+struct SectionHeaderView: View {
+    
+    var text: String
+    
+    var body: some View {
+        
+        HStack {
+            Text(text)
+                .font(.headline)
+                .foregroundStyle(Color.blue)
+                .background(Color.yellow)
+            Spacer()
+        }
+        .padding()
+        .background(Color.mint)
     }
 }
+
+
+
 
 #Preview {
     NavStackView()
--- SwiftUIAssignments/SwiftUIAssignmentsApp.swift
+++ SwiftUIAssignments/SwiftUIAssignmentsApp.swift
@@ -11,8 +11,8 @@ import SwiftUI
 struct SwiftUIAssignmentsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
-//            NavStackView()
+//            ContentView()
+            NavStackView()
         }
     }
 }
