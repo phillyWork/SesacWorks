cd9aee72b1a6b0b4d1465e76179ba790495e5629
philly___ham (함희돈) <98685150+phillyWork@users.noreply.github.com>
2023-11-17 22:42:55 +0900

    - H&VStack with Spacer - SheetPresentation for Button action with State variable

---
+++ SwiftUIAssignments/NavLinkLazyStack/NavStackView.swift
@@ -0,0 +1,18 @@
+//
+//  NavStackView.swift
+//  SwiftUIAssignments
+//
+//  Created by Heedon on 2023/11/17.
+//
+
+import SwiftUI
+
+struct NavStackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavStackView()
+}
