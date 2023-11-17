02b0365029868d8918c5823608c2a5806f380ef3
philly___ham (함희돈) <98685150+phillyWork@users.noreply.github.com>
2023-11-17 21:50:13 +0900

    Initial Commit

---
+++ SwiftUIAssignments/SwiftUIAssignmentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAssignmentsApp.swift
+//  SwiftUIAssignments
+//
+//  Created by Heedon on 2023/11/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAssignmentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
