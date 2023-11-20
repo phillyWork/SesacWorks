5396f182955a7212a9f3a70d50443ffd741e0de1
philly___ham (함희돈) <98685150+phillyWork@users.noreply.github.com>
2023-11-20 11:45:54 +0900

    Initial Commit

---
+++ SwiftUICoinOrderBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUICoinOrderBook
+//
+//  Created by Heedon on 2023/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUICoinOrderBook/SwiftUICoinOrderBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoinOrderBookApp.swift
+//  SwiftUICoinOrderBook
+//
+//  Created by Heedon on 2023/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoinOrderBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
